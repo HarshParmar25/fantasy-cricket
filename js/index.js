405383118cf0a57fbf73ed5a37212355d81e7fbc
HarshParmar25 <harshparmar2506@gmail.com>
2022-07-20 16:04:45 +0530

    commit

--- js/index.js
+++ js/index.js
@@ -9,27 +9,35 @@ let teams = [
     players: [],
   }),
 ];
-function toss() {
-  let team1 = document.querySelector("#team1").value;
-  let team2 = document.querySelector("#team2").value;
-  let result = document.getElementById("result");
+let showTossWinnerTeam = document.getElementById("result");
+
+function onClickTossButton() {
+  if(isValidateInput()) return;
   let toss = Math.floor(Math.random() * 2);
-  if (team2 == "" || team1 == "") {
-    result.innerHTML = "Please enter team name";
-  } else {
-    if (toss == 0) {
-      result.innerHTML = `${team1} won the toss and elected to bat first`;
-      teams[0].teamName = team1;
-      teams[1].teamName = team2;
-    } else {
-      result.innerHTML = `${team2} won the toss and elected to bat first`;
-      teams[0].teamName = team2;
-      teams[1].teamName = team1;
-    }
-    document.querySelector('#teamSelBtn').hidden = false;
-  }
-  return teams;
+  toss == 0 ? setTeamName(0) : setTeamName(1);
 }
+
+function isValidateInput() {
+  let { team1, team2 } = getTeamNamesFromInput();
+  if (team2 == "" || team1 == "") {
+    showTossWinnerTeam.innerHTML = "Please enter team name";
+    return true
+  }
+}
+
+function setTeamName(teamNumber) {
+  let { team1, team2 } = getTeamNamesFromInput();
+  teams[teamNumber].teamName = team1;
+  teams[1 - teamNumber].teamName = team2;
+  showTossWinnerTeam.innerHTML = `${teams[0].teamName} won the toss and elected to bat first`;
+  document.querySelector("#teamSelBtn").hidden = false;
+}
+
+ function getTeamNamesFromInput() {
+   let team1 = document.querySelector("#team1").value;
+   let team2 = document.querySelector("#team2").value;
+   return { team1, team2 };
+ }
 
 function clearTossPage() {
   localStorage.setItem("team1", JSON.stringify(teams[0]));
